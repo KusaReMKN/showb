6a011c5f3acfad898c5edee026a9e43cb4ab48ca
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2026-03-18 07:58:42 +0900

    Add 直近 24 時間

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ update()
 		${d.toLocaleDateString()} ${d.toLocaleTimeString()} 時点で
 		${w} W の電力を消費しています。`;
 	graph.src = `./graph.sh?${Math.random()}`;
+	daygraph.src = `./daygraph.sh?${Math.random()}`;
 	if (autoupdate.checked)
 		setTimeout(update, 15000);
 }
