d14cd1a353135b7fea841405c508de3a9d4cd50e
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2026-03-17 18:46:26 +0900

    Ignore requests not coming from 172.20.40.3

--- recv.sh
+++ recv.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
+if [ "$REMOTE_ADDR" != "172.20.40.3" ]
+then
+	printf 'HTTP/1.1 200 Forbidden\r\n'
+	printf 'Content-Type: text/plain; charset=utf-8\r\n'
+	printf '\r\n'
+	printf 'Ignore\r\n'
+	exit 1
+fi
+
 datafile="./data.txt"
 
 printf 'HTTP/1.1 200 OK\r\n'
