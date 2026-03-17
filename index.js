66256038c7aaeb58ea50265ba313b7cc86e6cd47
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2026-03-17 19:01:32 +0900

    Move inline CSS and JS to external files

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+async function
+update()
+{
+	const last = await fetch('./last.sh').then(r => r.text());
+	const [ t, w, i ] = last.split('\t').map(e => +e);
+	const d = new Date(t * 1000);
+	current.textContent = `
+		${d.toLocaleDateString()} ${d.toLocaleTimeString()} 時点で
+		${w} W の電力を消費しています。`;
+	if (autoupdate.checked)
+		setTimeout(update, 15000);
+}
+
+autoupdate.addEventListener('change', _ => autoupdate.checked && update());
+update();
